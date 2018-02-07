e1afa151cc332269b219b55458847e20360ae327
贾伟川 <wellschuan@gmail.com>
2018-02-08 00:40:28 +0800

    Update BlockHeader, add bitcoin block description

--- BlockHeader.go
+++ BlockHeader.go
@@ -13,3 +13,28 @@ type BlockHeader struct {
 
 	Nonce uint32
 }
+
+
+// bitcoin block
+// 
+// Field    Description    Size
+// Magic no    value always 0xD9B4BEF9    4 bytes
+// Blocksize    number of bytes following up to end of block    4 bytes
+// Blockheader    consists of 6 items    80 bytes
+// Transaction counter    positive integer VI = VarInt    1 - 9 bytes
+// transactions    the (non empty) list of transactions    -many transactions
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
